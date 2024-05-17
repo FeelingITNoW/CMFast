732c693a30d9f0b92ed795adb051c2b83a15bd25
APDumo123 <adrian.dumosmog@deya.app>
2024-05-18 01:03:10 +0800

    edited camera movement and camera position

--- CameraMovement.gd
+++ CameraMovement.gd
@@ -8,6 +8,7 @@ const rot_factor = .01
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
+	
 	if (Input.is_action_just_pressed(("ui_mouse_click"))): 
 		rotating = true 
 		prev_mouse_position = get_viewport().get_mouse_position();
@@ -18,8 +19,10 @@ func _process(delta):
 	
 	if (rotating):
 		next_mouse_position = get_viewport().get_mouse_position()
-		rotate_y((next_mouse_position.x - prev_mouse_position.x) * rot_factor * delta)
-		rotate_z((next_mouse_position.y - prev_mouse_position.y) * rot_factor * delta)
+		#rotate_y((next_mouse_position.x - prev_mouse_position.x) * rot_factor * delta)
+		#rotate_z((next_mouse_position.y - prev_mouse_position.y) * rot_factor * delta)
+		var translation_vector = Vector3(next_mouse_position.x - prev_mouse_position.x, 0, next_mouse_position.y -prev_mouse_position.y) * rot_factor
+		translate(translation_vector) 
 		
 
 
