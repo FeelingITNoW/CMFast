81c53677e6e6510fe3778f58dd20c8640dd62830
personal <adumosmog@gmail.com>
2024-06-04 18:49:29 +0800

    added recenter camera button and drag camera button

--- CameraMovement.gd
+++ CameraMovement.gd
@@ -6,17 +6,8 @@ var next_mouse_position
 var velocity
 const rot_factor = .01 
 
-# Called when the node enters the scene tree for the first time.
+
 func _process(delta):
-	
-	if (Input.is_action_just_pressed(("ui_mouse_click"))): 
-		rotating = true 
-		prev_mouse_position = get_viewport().get_mouse_position();
-	
-	if (Input.is_action_just_released(('ui_mouse_click'))):
-		rotating = false 
-		next_mouse_position = get_viewport().get_mouse_position()
-	
 	if (rotating):
 		next_mouse_position = get_viewport().get_mouse_position()
 		#rotate_y((next_mouse_position.x - prev_mouse_position.x) * rot_factor * delta)
@@ -24,15 +15,15 @@ func _process(delta):
 		var translation_vector = Vector3(next_mouse_position.x - prev_mouse_position.x, 0, next_mouse_position.y -prev_mouse_position.y) * rot_factor
 		translate(translation_vector) 
 	
-
-
-
-
-func _on_button_pressed():
-	print('pre button press', self.position)
+func _on_recenter_camera_button_pressed():
 	var position = self.position
 	var rotation = self.rotation
-	print('rotation', rotation)
 	var oppositeVector = Vector3(-1 * self.position.x, -1 * self.position.y, -1 * self.position.z)
 	self.translate(oppositeVector)
-	print('post button press', self.position)
+
+func _on_drag_camera_button_button_up():
+	rotating = false
+	
+func _on_drag_camera_button_button_down():
+	rotating = true
+	prev_mouse_position = get_viewport().get_mouse_position()
